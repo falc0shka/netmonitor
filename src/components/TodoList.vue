d8387e9b13e0a211a4d7ddeca3888b837f6eea81
Sergey Sokolov <SSokolov@sintez-group.sintez.corp>
2022-09-09 22:16:19 +0300

    Add all files

---
+++ src/components/TodoList.vue
@@ -0,0 +1,25 @@
+<script setup>
+  import TodoItem from './TodoItem.vue'
+
+  import { reactive } from 'vue'
+
+  const state = reactive({ count: 0 })
+
+  function increment() {
+    state.count++
+  }
+
+
+</script>
+
+<template>
+  <div>
+    <h2>To Do List for Netmonitor</h2>
+    <button @click="increment">
+      {{ state.count }}
+    </button>
+    <ul>
+      <TodoItem />
+    </ul>
+  </div>
+</template>
