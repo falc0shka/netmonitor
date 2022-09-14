9f549abdba73f67b5f2db4ac297a2b0b03c61fa6
falc0shka <falc0shka@gmail.com>
2022-09-14 23:05:46 +0300

    Add todo module

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,13 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 import HelloWorld from './components/HelloWorld.vue'
-import TodoList from './components/TodoList.vue'
+import Dashboard from './components/Dashboard.vue'
 </script>
 
 <template>
-  <h1>Netmonitor app v1.0.0</h1>
-  <TodoList />
+  <header>Netmonitor app v1.0.0</header>
+  <h1>Dashboard</h1>
+  <Dashboard />
   <HelloWorld msg="Hello world module" class="baz boo"/>
 </template>
 
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,40 +1,71 @@
 <script setup>
-  import TodoItem from './TodoItem.vue'
+  import TodoListItem from './TodoListItem.vue'
 
   import { ref, reactive, computed} from 'vue'
 
-  const state = reactive({ count: 0 })
-
-  function increment() {
-    state.count++
-  }
-
-  const myObject = reactive(
-  [
+  const todoListItems = reactive([
     {
       id: 1,
-      action: 'Buy bread'
+      action: 'Buy bread',
+      done: true
     },
     {
       id: 2,
-      action: 'Buy coke'
+      action: 'Buy coke',
+      done: false
     },
     {
       id: 3,
-      action: 'Buy cheese'
+      action: 'Buy cheese',
+      done: true
     }
   ]
   )
+  
+  const addItemInput = ref('')
+  let increment = 4;
+  function todoListAddItem() {
+    if (addItemInput.value) {
+      todoListItems.push({
+        id: increment++,
+        action: addItemInput.value
+      })
+      addItemInput.value = '';
+    }    
+  }
+
+  const todoItemSelect = ref('all')
+  const selected = ref('false')
 
 </script>
 
 <template>
-  <div>
     <h2>ToDo List module</h2>
-    <button @click="increment">{{ state.count }}</button>
+    <!-- <button @click="increment">{{ state.count }}</button> -->
+    <div><input type="text" v-model.lazy="addItemInput" placeholder="Input task name" />
+    <button @click="todoListAddItem">Add task</button>
+    </div>
+    
+    <!-- <label for="todoItemSelect">Show items ...</label> -->
+    
+      <select id="todoItemSelect" v-model="todoItemSelect">
+        <option disabled value="">Выберите один из вариантов</option>
+        <option value="all">Show all</option>
+        <option value="false">Only undone</option>
+        <option value="true">Only done</option>
+      </select>
+    
+    <div>Selected: {{ todoItemSelect }}</div>
+
     <ul>
-      <li  v-for="(value, key) of myObject" :key="value.id">{{value.id}}: {{value.action}}</li>
-      <TodoItem />
+      <template v-for="(value, index) of todoListItems" :key="value.id">
+        <todo-list-item     v-if="value.done.toString()===todoItemSelect||todoItemSelect==='all'" 
+          :todoAction="value.action"     
+          :todoItemStatus = "value.done"
+          @todoItemChecked = "value.done = true"
+          @todoItemUnchecked = "value.done = false" 
+          @todoRemoveItem="todoListItems.splice(index,1)"
+        ></todo-list-item>
+    </template>
     </ul>
-  </div>
 </template>
