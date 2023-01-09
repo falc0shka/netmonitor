fe68435a93bd354418bfed7ff762efc9ac2c4dc2
falc0shka <falc0shka@gmail.com>
2023-01-09 16:33:30 +0300

    Fix authentication

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -16,15 +16,21 @@ const password = ref('');
       <div class="row">
         <q-card square bordered class="q-pa-lg shadow-1">
           <q-card-section>
-            <q-form class="q-gutter-md">
+            <q-form
+              class="q-gutter-md"
+              @submit.prevent="authStore.login({ email, password })"
+            >
               <q-input
                 square
                 filled
                 clearable
                 v-model="email"
                 type="email"
-                label="email"
+                label="email *"
                 autocomplete="email"
+                :rules="[
+                  (val) => (val && val.length > 0) || 'Please type email',
+                ]"
               />
               <q-input
                 square
@@ -32,21 +38,23 @@ const password = ref('');
                 clearable
                 v-model="password"
                 type="password"
-                label="password"
+                label="password *"
                 autocomplete="current-password"
+                :rules="[
+                  (val) => (val && val.length > 0) || 'Please type password',
+                ]"
+              />
+              <q-btn
+                unelevated
+                color="light-blue-7"
+                size="lg"
+                class="full-width"
+                label="Login"
+                type="submit"
               />
             </q-form>
           </q-card-section>
-          <q-card-actions class="q-px-md">
-            <q-btn
-              unelevated
-              color="light-blue-7"
-              size="lg"
-              class="full-width"
-              label="Login"
-              @click="authStore.login({ email, password })"
-            />
-          </q-card-actions>
+          <q-card-actions class="q-px-md"> </q-card-actions>
           <q-card-section class="text-center">
             <router-link to="/registration">Created an Account</router-link>
           </q-card-section>
--- src/pages/RegistrationPage.vue
+++ src/pages/RegistrationPage.vue
@@ -17,15 +17,22 @@ const name = ref('');
       <div class="row">
         <q-card square bordered class="q-pa-lg shadow-1">
           <q-card-section>
-            <q-form class="q-gutter-md">
+            <q-form
+              class="q-gutter-md"
+              @submit.prevent="
+                authStore.registration({ email, password, name })
+              "
+            >
               <q-input
                 square
                 filled
                 clearable
                 v-model="name"
                 type="name"
-                label="name"
-                autocomplete="name"
+                label="name *"
+                :rules="[
+                  (val) => (val && val.length > 0) || 'Please type name',
+                ]"
               />
               <q-input
                 square
@@ -33,7 +40,10 @@ const name = ref('');
                 clearable
                 v-model="email"
                 type="email"
-                label="email"
+                label="email *"
+                :rules="[
+                  (val) => (val && val.length > 0) || 'Please type email',
+                ]"
                 autocomplete="email"
               />
               <q-input
@@ -42,21 +52,22 @@ const name = ref('');
                 clearable
                 v-model="password"
                 type="password"
-                label="password"
+                label="password *"
                 autocomplete="current-password"
+                :rules="[
+                  (val) => (val && val.length > 0) || 'Please type password',
+                ]"
+              />
+              <q-btn
+                unelevated
+                color="light-blue-7"
+                size="lg"
+                class="full-width"
+                label="Create an account"
+                type="submit"
               />
             </q-form>
           </q-card-section>
-          <q-card-actions class="q-px-md">
-            <q-btn
-              unelevated
-              color="light-blue-7"
-              size="lg"
-              class="full-width"
-              label="Create an account"
-              @click="authStore.registration({ email, password, name })"
-            />
-          </q-card-actions>
           <q-card-section class="text-center">
             <router-link to="/login">Back to login form</router-link>
           </q-card-section>
